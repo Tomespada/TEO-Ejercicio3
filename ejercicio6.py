751ea615c6a8b18faae95e83b35c38e09a037f33
Tomespada019 <tomasespadagarcia@gmail.com>
2025-10-22 11:35:47 +0200

    Cambio 2

--- ejercicio6.py
+++ ejercicio6.py
@@ -8,6 +8,20 @@ Precios de las entradas para personas con edades 8, 18, 25, 44, 64, 65, 70
 def calcula_precios(precio_normal, edades):
     precios = []
     for edad in edades:
-        precio = calcula_precios(precio_normal,edad)
-        precios.add(precio)
+        precio = calcula_precio(precio_normal, edad)
+        precios.append(precio)
     return precios
+
+def calcula_precio(precio_normal,edad):
+    if edad <= 18 or edad >=65:
+        precio = precio_normal/2
+    else:
+        precio = precio_normal
+    return precio 
+
+if __name__ == '__main__':
+    edades = [8,18,25,44,64,65,70]
+    precio_normal=6
+    precios = calcula_precios(precio_normal,edades)
+    for edad, precio in zip(edades,precios):
+        print(f"El precio para {edad} años es {precio}€")
